fe80052c964d9919ccade0638b3a81e2dba2c2c4
Neel-kadia <neelkadia235@gmail.com>
2022-09-29 16:54:47 -0700

    Add DoWhileExample Java File

---
+++ DoWhileExample.java
@@ -0,0 +1,17 @@
+/**
+	This Programme is About to Print 1 to 10 Numbers using DoWhile Loop.
+	COPYRIGHT (C) 2022 QDEV TECHNOLAB. All Rights Reserved.
+	@version 1.0.0
+*/
+
+public class DoWhileExample{
+
+	public static void main(String[] args){
+		int a = 1;	// Initialization
+		do{
+			System.out.println(a);
+			a++;	// increment
+		}while (a <= 10);	// condition 
+	}
+
+}
