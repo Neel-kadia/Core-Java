25d4e204068ffed82af2c397c739d7183db92b70
Neel-kadia <neelkadia235@gmail.com>
2022-09-29 16:36:25 -0700

    Add WhileExample Java File

---
+++ WhileExample.java
@@ -0,0 +1,17 @@
+/**
+	This Programme is About to Print 1 to 10 Numbers using While Loop.
+	COPYRIGHT (C) 2022 QDEV TECHNOLAB. All Rights Reserved.
+	@version 1.0.0
+*/
+
+public class WhileExample{
+
+	public static void main(String[] args){
+		int a = 1;		// Initialization
+		while(a <= 10){
+			System.out.println(a);
+			a++;		// Increment
+		}
+	}
+
+}
