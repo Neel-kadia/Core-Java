523e0982a4996f6fb7d9063c905772a6aa42e085
Neel-kadia <neelkadia235@gmail.com>
2022-09-29 12:57:11 -0700

    Print Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+	This Programme is Print to HelloWorld.
+    COPYRIGHT (C) 2022 QDEV TECHNOLAB.All Rights Reserved.
+    @version 1.0.0
+*/
+public class HelloWorld{
+
+	public static void main(String[] args){
+		System.out.println("Hello World");  // Print Hello World
+	}
+
+}
