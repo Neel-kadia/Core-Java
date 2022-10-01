f2ff01e0ba04fef565e02a788e7f3e3aaebfb79a
Neel-kadia <neelkadia235@gmail.com>
2022-09-30 18:48:38 -0700

    Add java file

---
+++ Addition.java
@@ -0,0 +1,27 @@
+/**
+ *	This program is about the parameterized constructor.
+ *	Passed two integer parameters and applied a mathematical formula
+ *	COPYRIGHT (C) 2022 QDEV TECHNOLAB. All Rights Reserved.
+ *	@author Neel
+ *	@version 1.0.0
+ */
+class Add {
+
+	int number3;
+	// Created parameterized constructor.
+	Add(int a, int b) {
+		number3 = a + b; 
+	}
+	void display() {
+		System.out.println(number3);
+	}
+
+}
+public class Addition {
+
+	public static void main(String[] args) {
+		Add a1 = new Add(10, 20); // The constructor is invoked while creating an object of the Add class.
+		a1.display();
+	}
+
+}
---
+++ BreakExample.java
@@ -0,0 +1,24 @@
+/**
+ *	This program is about continuing and breaking statements.
+ *	When for loop is running then check the condition if the value of a is equal to 4 then continue the program
+ *	and if the value of a is equal to 7 then break the program.
+ *	COPYRIGHT (C) 2022 QDEV TECHNOLAB. All Rights Reserved.
+ *	@author Neel
+ *	@version 1.0.0
+ */
+
+public class BreakExample {
+
+	public static void main(String[] args) {
+		for(int a=1; a<=10; a++) {
+			if(a == 4) {
+				continue; // if value of a is equal to 4 then continue the program 
+			}
+			if(a == 7) {
+				break; // if value of a is equal to 7 then break the program 
+			}
+			System.out.println(a);
+		}
+	}
+
+}
